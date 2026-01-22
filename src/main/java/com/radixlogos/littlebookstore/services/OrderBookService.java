b16e70aa26eae4513a01f02948e8431bd1abefa8
rubens <rubenshds@gmail.com>
2026-01-22 14:34:01 -0300

    merge

---
+++ src/main/java/com/radixlogos/littlebookstore/services/OrderBookService.java
@@ -0,0 +1,4 @@
+package com.radixlogos.littlebookstore.services;
+
+public class OrderBookService {
+}
---
+++ src/main/java/com/radixlogos/littlebookstore/services/exceptions/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.radixlogos.littlebookstore.services.exceptions;
+
+public class OrderNotFoundException extends RuntimeException {
+  public OrderNotFoundException(String message) {
+    super(message);
+  }
+}
